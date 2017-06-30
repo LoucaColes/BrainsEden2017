be57f928f1b49660a8d021c28730e001c6ebbb52
Nickstu14 <nickstu14@hotmail.co.uk>
2017-06-30 20:32:07 +0100

    ui again

--- Brains Eden Project/Brains Eden 2017/Assets/UI/UiBar.cs
+++ Brains Eden Project/Brains Eden 2017/Assets/UI/UiBar.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UiBar : MonoBehaviour {
+public class UiBar : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Image m_bar;
+    public float m_Max = 100f;
+    
+    public float m_current = 0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        m_current = m_Max;
+
+    }
+
+
+
+  
+
+
+    void Decreacebar(float _dec)
+    {
+        m_current -= _dec;
+        SetBar(m_current / m_Max);
+    }
+
+    void SetBar(float _amount)
+    {
+        if(_amount > 1 || _amount < 0)
+        {
+            return;
+        }
+        m_bar.fillAmount = m_current;
+    }
 }
