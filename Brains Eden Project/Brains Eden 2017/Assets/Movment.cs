39d68258ed1eb1800441843fbb0e2ee7b487a4f7
Gualt_ <gualtyphone@me.com>
2017-07-01 09:18:56 +0100

    Merge branch 'master' of https://github.com/LoucaColes/BrainsEden2017

--- Brains Eden Project/Brains Eden 2017/Assets/Movment.cs
+++ Brains Eden Project/Brains Eden 2017/Assets/Movment.cs
@@ -16,7 +16,7 @@ public class Movment : MonoBehaviour {
     private void Update()
     {
         // RB.AddForce(new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical")) *Time.deltaTime*(speed*10));
-        transform.position+= new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * (speed/10);
+        transform.position += new Vector3(Input.GetAxis("Horizontal" + ControllerNum), 0, Input.GetAxis("Vertical" + ControllerNum)) * Time.deltaTime * (speed/10);
 
         float t_rx = Input.GetAxis("RHorizontal"+ ControllerNum);
         float t_ry = Input.GetAxis("RVertical"+ ControllerNum);
