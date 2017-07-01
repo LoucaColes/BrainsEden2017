eba1acc22b2b7a3dc76e4a209bc8005d019769f9
LoucaColes <coleslouca@yahoo.co.uk>
2017-07-01 09:15:59 +0100

    Ignore Players own collider
    Ignore Players own collider and added prefabs.

--- Brains Eden Project/Brains Eden 2017/Assets/Scripts/PlayerAiming.cs
+++ Brains Eden Project/Brains Eden 2017/Assets/Scripts/PlayerAiming.cs
@@ -6,10 +6,15 @@ public class PlayerAiming : MonoBehaviour
 {
     private Transform m_rayPoint;
     private Transform m_testPlayer;
+
+    private Collider m_colliderRadius;
     public GameObject m_particle;
 
     public float m_rayDistance;
     public float m_speed;
+
+    public int m_layerId;
+    private int m_layerMask;
 
     private float m_bezierTime;
 
@@ -17,6 +22,10 @@ public class PlayerAiming : MonoBehaviour
     private void Start()
     {
         m_rayPoint = transform.FindChild("RayPoint");
+        m_colliderRadius = transform.FindChild("ColliderRadius").GetComponent<Collider>();
+        transform.FindChild("ColliderRadius").gameObject.layer = m_layerId;
+        m_layerMask = 1 << 12;
+        m_layerMask = ~m_layerMask;
         m_bezierTime = 0;
         m_particle.transform.position = m_rayPoint.position;
     }
@@ -85,9 +94,10 @@ public class PlayerAiming : MonoBehaviour
         RaycastHit t_hit;
         Vector3 t_dir = m_rayPoint.forward;
 
-        if (Physics.Raycast(m_rayPoint.position, t_dir, out t_hit))
+        if (Physics.Raycast(m_rayPoint.position, t_dir, out t_hit, Mathf.Infinity, m_layerMask))
         {
-            if (t_hit.collider.tag == "Player")
+            print("hit something");
+            if (t_hit.collider.tag == "ColRadius")
             {
                 print("Player Hit");
                 m_testPlayer = t_hit.collider.transform;
