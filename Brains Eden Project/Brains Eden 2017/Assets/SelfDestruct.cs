6c0e71eaa33e330f3c245f80e7f359e6590a317a
d2-pamphilon <danpamphilon@yahoo.co.uk>
2017-07-01 16:15:14 +0100

    dif colours for players

--- Brains Eden Project/Brains Eden 2017/Assets/SelfDestruct.cs
+++ Brains Eden Project/Brains Eden 2017/Assets/SelfDestruct.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SelfDestruct : MonoBehaviour {
 
-	public void deathTimerStart () {  Destroy(gameObject, 1);
+	public void deathTimerStart () {  Destroy(gameObject, 0.5f);
 	}
 	
 }
