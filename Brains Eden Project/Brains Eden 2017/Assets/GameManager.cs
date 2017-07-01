cf7c394c4377b25b47acdfe7046f4fc68623a5d0
Gualt_ <gualtyphone@me.com>
2017-07-01 09:18:16 +0100

    GameManager

--- Brains Eden Project/Brains Eden 2017/Assets/GameManager.cs
+++ Brains Eden Project/Brains Eden 2017/Assets/GameManager.cs
@@ -2,15 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState
+{
+    Selection,
+    Game,
+    GameOver
+}
+
 public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject PlayerPrefab;
+
+    public GameState state = GameState.Selection;
+
+    public GameObject[] Players;
+
+    public int maxPlayers;
 
 	// Use this for initialization
 	void Start () {
-		
+        DontDestroyOnLoad(this);
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+        switch (state)
+        {
+            case GameState.Selection:
+                UpdateSelection();
+                break;
+            case GameState.Game:
+                UpdateGame();
+                break;
+            case GameState.GameOver:
+                UpdateGameOver();
+                break;
+        }
 	}
+
+    void UpdateSelection()
+    {
+        for (int i = 0; i < maxPlayers; i++)
+        {
+            if (Input.GetButtonDown("joystick " + i + " button 0"))
+            {
+
+            }
+        }
+
+    }
+
+    void UpdateGame()
+    {
+
+    }
+
+    void UpdateGameOver()
+    {
+
+    }
 }
